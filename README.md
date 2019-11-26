375a70613c2e2474b1e3f4303ec2b538f2db33e8
jacobhsu <jacob.hsu.tw@gmail.com>
2019-11-26 11:51:04 +0800

    https://zh-hant.reactjs.org/docs/hooks-overview.html

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ yarn start
 ## Hook
 
 Hook 是 React 16.8 中增加的新功能。它讓你不必寫 class 就能使用 state 以及其他 React 的功能。
+
+Hook 是 function，他讓你可以從 function component「hook into」React state 與生命週期功能。
+
+## Effect Hook
+
+你從前可能在 React component 做過 fetch 資料、訂閱、或手動改變 DOM。我們稱這些操作「`side effect`」（或簡稱 effect）因為他們可以影響其他 component 且在 render 期間無法完成。
+
+Effect Hook useEffect 在 function component 中加入運作 side effect 的能力。他和 `componentDidMount`，`componentDidUpdate`，與 `componentWillUnmount` 有著同樣的宗旨，但整合進一個單一的 API。
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
 function App() {
   // 宣告一個新的 state 變數，我們稱作為「count」。
   const [count, setCount] = useState(0);
+
+  // 與 componentDidMount 和 componentDidUpdate 類似：
+  useEffect(() => {
+    // 使用瀏覽器 API 更新文件標題
+    document.title = `You clicked ${count} times`;
+  });
+  
+
   return (
     <div>
       <p>You clicked {count} times</p>
